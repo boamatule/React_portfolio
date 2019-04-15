3e4836e8fb7a54c49a304b3747395eb18fc2d19b
boa <boa.matule@gmail.com>
2019-04-15 16:26:36 +0200

    installed the BrouserRouter from React Router. Also added two new files(about.jsx and projects.jsx) in src

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
+import { BrowserRouter } from 'react-router-dom'
 import React from "react"
 import ReactDOM from "react-dom"
 import Hello from "./Hello"
 import Header from "./Header"
 import Footer from "./Footer"
-import "./css/tailwind.css";
+import Projects from "./Projects"
+import About from "./About"
+import "./css/tailwind.css"
+
 
 
 const App = () => {
     return (
-        <div className="min-h-screen">
-        <div className="flex flex-col w-full fixed pin-l pin-y">
+        <div className="page-wrapper">
+        <div className="page-content">
             <Header />
-            <div className="px-4 pt-4 flex-1 overflow-y-scrol">
+            <div className="content-wrapper">
                 <Hello />
             </div>
             <Footer />
@@ -20,4 +24,8 @@ const App = () => {
   )  
 };
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('app'));
