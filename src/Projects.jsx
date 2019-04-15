4695f1c51b47a133d1999ac8da3486ff964b8f40
boa <boa.matule@gmail.com>
2019-04-15 20:23:00 +0200

    re-styling the project list and created ProjectCard file

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import axios from "axios"
+import ProjectCard from "./ProjectCard"
 
 
 class Projects extends Component {
@@ -20,7 +21,7 @@ class Projects extends Component {
                 })
             })
     }
-    
+
     render() {
         const projects = this.state.projects
         let projectsList 
@@ -28,11 +29,10 @@ class Projects extends Component {
         if (projects.length > 0) {
             projectsList = projects.map(project => {
                 return (
-                    <div key={project.id}>
-                        <h3 className="content-text">
-                            {project.name}
-                        </h3>
+                    <div key={project.id} className="min-h-900 my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+                            <ProjectCard project={project} />
                     </div>
+ 
                 )
             })
         }
